d7b83047d9e42d794737e2f55cdc8110a0b0a292
croductive <croductive@gmail.com>
2021-01-12 18:56:28 +0100

    stringToArray

---
+++ strings/35-8kyu-stringToArray.js
@@ -0,0 +1,15 @@
+function stringToArray(string) {
+  // code code code
+  return string.split(" ");
+}
+
+console.log(stringToArray("Robin Singh"), ["Robin", "Singh"]);
+console.log(stringToArray("I love arrays they are my favorite"), [
+  "I",
+  "love",
+  "arrays",
+  "they",
+  "are",
+  "my",
+  "favorite",
+]);

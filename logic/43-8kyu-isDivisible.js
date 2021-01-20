d099cb02e0c79968ebee641b8b923b2581dc07d9
croductive <croductive@gmail.com>
2021-01-20 12:52:36 +0100

    division x y

---
+++ logic/43-8kyu-isDivisible.js
@@ -0,0 +1,9 @@
+function isDivisible(n, x, y) {
+  if (n % x == 0 && n % y == 0) return true;
+  else return false;
+}
+
+console.log(isDivisible(3, 3, 4), false);
+console.log(isDivisible(12, 3, 4), true);
+console.log(isDivisible(8, 3, 4), false);
+console.log(isDivisible(48, 3, 4), true);

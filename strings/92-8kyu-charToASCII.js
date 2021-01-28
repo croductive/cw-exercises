036191fb2eadb6984186c9bc17cf74cb8850a541
croductive <croductive@gmail.com>
2021-01-28 18:13:50 +0100

    92 8kyu reverse String

---
+++ strings/92-8kyu-charToASCII.js
@@ -0,0 +1,10 @@
+function charToASCII(c) {
+  // Code here
+  return c.charCodeAt();
+}
+
+console.log(charToASCII("a"), 97);
+console.log(charToASCII("h"), 104);
+console.log(charToASCII("!"), 33);
+console.log(charToASCII("$"), 36);
+console.log(charToASCII("~"), 126);
---
+++ strings/93-8kyu-reverseString.js
@@ -0,0 +1,6 @@
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+console.log(reverseString("test"), "tset");
+console.log(reverseString("nice"), "ecin");

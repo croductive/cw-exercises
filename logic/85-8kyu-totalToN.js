ce48b308ddb7c589ad3120da47aea044bd11255b
croductive <croductive@gmail.com>
2021-01-28 17:13:40 +0100

    85 sum to n

---
+++ logic/85-8kyu-totalToN.js
@@ -0,0 +1,21 @@
+function total(num) {
+  // TODO: complete me
+  return (num * (num + 1)) / 2;
+}
+
+console.log(total(10), 55, "Remember to sum all values from 1 to n inclusive");
+console.log(
+  total(123),
+  7626,
+  "Remember to sum all values from 1 to n inclusive"
+);
+console.log(
+  total(1000),
+  500500,
+  "Remember to sum all values from 1 to n inclusive"
+);
+console.log(
+  total(54321),
+  1475412681,
+  "Remember to sum all values from 1 to n inclusive"
+);

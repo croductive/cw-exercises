6440bd4c6d3bec6ac53185970bbb35bf3e755314
croductive <croductive@gmail.com>
2021-01-29 18:36:05 +0100

    99 is isogram

---
+++ 99--7kyu-string-isIsogram.js
@@ -0,0 +1,10 @@
+function isIsogram(str) {
+  return !/(\w).*\1/i.test(str);
+}
+
+console.log(isIsogram("Dermatoglyphics"), true);
+console.log(isIsogram("isogram"), true);
+console.log(isIsogram("aba"), false, "same chars may not be adjacent");
+console.log(isIsogram("moOse"), false, "same chars may not be same case");
+console.log(isIsogram("isIsogram"), false);
+console.log(isIsogram(""), true, "an empty string is a valid isogram");

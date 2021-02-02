0a55f9ac0b3180f486d6a88dcb4ac4dd1b05bb8d
croductive <croductive@gmail.com>
2021-02-02 23:42:52 +0100

    109 array to string binary value

---
+++ 109-7kyu-array-binaryArrayToNumber.js
@@ -0,0 +1,9 @@
+const binaryArrayToNumber = (arr) => {
+  // your code
+  return parseInt(arr.join(""), 2);
+};
+
+console.log(binaryArrayToNumber([0, 0, 0, 1]), 1);
+console.log(binaryArrayToNumber([0, 0, 1, 0]), 2);
+console.log(binaryArrayToNumber([1, 1, 1, 1]), 15);
+console.log(binaryArrayToNumber([0, 1, 1, 0]), 6);
